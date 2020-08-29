65c4fae56b18734a65a6cdd259997a9b688e7f95
FH337 <thedayuwana@gmail.com>
2020-08-29 21:19:17 +0700

    Initial commit

---
+++ app/src/main/java/com/fingerprintauth/FingerprintHandler.java
@@ -0,0 +1,50 @@
+package com.fingerprintauth;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.CancellationSignal;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
+
+
+
+
+/**
+ * Created by reale on 25/11/2016.
+ */
+public class FingerprintHandler extends FingerprintManager.AuthenticationCallback {
+
+    private Context context;
+    private Intent intent = null;
+
+    public FingerprintHandler(Context context) {
+        this.context = context;
+    }
+
+    public void startAuthentication(FingerprintManager fingerprintManager, FingerprintManager.CryptoObject cryptoObject) {
+        CancellationSignal cenCancellationSignal = new CancellationSignal();
+        if(ActivityCompat.checkSelfPermission(context, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED)
+            return;
+        fingerprintManager.authenticate(cryptoObject,cenCancellationSignal,0,this,null);
+
+    }
+
+    @Override
+    public void onAuthenticationFailed() {
+        super.onAuthenticationFailed();
+        Toast.makeText(context, "Fingerprint Authentication failed!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+        super.onAuthenticationSucceeded(result);
+        Toast.makeText(context, "Fingerprint Authentication success!", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(context,DisplayMessageActivity.class);
+        context.startActivity(intent);
+
+    }
+
+}
